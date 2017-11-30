df0c9f9af4f47fd79a3e36e97e92e0fdf2f69e1a
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-11-30 20:10:19 +0000

    Allow transactions to be saved to csv file

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,35 +1,52 @@
+require 'date'
+require 'csv'
+
 class Bank
 
   AMOUNT = 0
 
-  attr_accessor :account, :customer, :balance
+  attr_accessor :account, :transaction, :balance
 
   def initialize
-    @customer = { :name => [], :date => [], :credit => [], :debit => [], :balance => [] }
     @balance = AMOUNT
+    @transaction = { :date => "", :credit => "", :debit => "", :customer_balance => "" }
   end
 
   def add_name(name)
-    @customer[:name] = name
+    @transaction[:name] = name
+  end
+
+  def add_date
+    date = Date.today
+    @transaction[:date] = date
   end
 
   def add_credit(credit)
-    @customer[:credit] = credit
+    add_date
+    @transaction[:debit] = ""
+    @transaction[:credit] = credit
     @balance += credit
+    @transaction[:customer_balance] = @balance
+    save_account
   end
 
   def withdraw(debit)
-    @customer[:debit] = debit
+    add_date
+    @transaction[:credit] = ""
+    @transaction[:debit] = debit
     @balance -= debit
+    @transaction[:customer_balance] = @balance
+    save_account
   end
 
   def save_account
-    file = File.open("account.csv", "w")
-    @customer.each do |detail|
-      csv_line = @customer.join(",")
-      file.puts csv_line
+    CSV.open("../account.csv", "w") do |csv|
+      csv << @transaction.values
     end
-    file.close
   end
 
 end
+
+bank = Bank.new
+bank.add_credit(100)
+bank.withdraw(10)
