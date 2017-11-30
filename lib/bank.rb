492428c0372e2c6c2beca51f46044add1af6c844
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-11-30 21:03:21 +0000

    Print statement

--- lib/bank.rb
+++ lib/bank.rb
@@ -4,6 +4,7 @@ require 'csv'
 class Bank
 
   AMOUNT = 0
+  HEADER = "date || credit || debit || balance"
 
   attr_accessor :account, :transaction, :balance
 
@@ -23,19 +24,19 @@ class Bank
 
   def add_credit(credit)
     add_date
-    @transaction[:debit] = ""
-    @transaction[:credit] = credit
+    @transaction[:debit] = " "
+    @transaction[:credit] = convert_to_currency(credit)
     @balance += credit
-    @transaction[:customer_balance] = @balance
+    @transaction[:customer_balance] = convert_to_currency(@balance)
     save_account
   end
 
   def withdraw(debit)
     add_date
-    @transaction[:credit] = ""
-    @transaction[:debit] = debit
+    @transaction[:credit] = " "
+    @transaction[:debit] = convert_to_currency(debit)
     @balance -= debit
-    @transaction[:customer_balance] = @balance
+    @transaction[:customer_balance] = convert_to_currency(@balance)
     save_account
   end
 
@@ -45,8 +46,19 @@ class Bank
     end
   end
 
+  def convert_to_currency(integer)
+    format('%.2f', integer)
+  end
+
+  def print_statement
+    p HEADER
+    CSV.foreach("../account.csv") do |row|
+    p row.join("||")
+    end
+  end
 end
 
 bank = Bank.new
 bank.add_credit(100)
 bank.withdraw(10)
+bank.print_statement
